96025a4b8325e64d0b066eff6d7d1653be148427
HoBeom <hobeom@gmail.com>
2020-09-14 09:48:22 +0900

    ADD ipc=host option

--- run-openpose-docker.sh
+++ run-openpose-docker.sh
@@ -1,1 +1,1 @@
-sudo docker run --gpus all --device=/dev/video0:/dev/video0 --net=host -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --name openpose -it openpose:latest /bin/bash
+sudo docker run --gpus all --device=/dev/video0:/dev/video0 --net=host -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix --privileged --ipc=host --name openpose -it openpose:latest /bin/bash
